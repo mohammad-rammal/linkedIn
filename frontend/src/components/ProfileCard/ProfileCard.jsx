4c7c63ea7b7e65783332be568105b7a477170377
mohammad-rammal <mohammad.rammal@hotmail.com>
2025-06-06 13:00:38 +0300

    Add Advertisement and ProfileCard components with Card wrapper
    - Implemented Advertisement component featuring a landscape image, profile image, and call-to-action button.
    - Created ProfileCard component displaying user information with a similar layout to Advertisement.
    - Introduced reusable Card component to encapsulate common styling and structure for both Advertisement and ProfileCard.

---
+++ frontend/src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,33 @@
+import Card from "../Card/Card";
+import landscape from "../../assets/images/landscapeImage.jpg";
+import profileImage from "../../assets/images/profileImage.png";
+
+const ProfileCard = () => {
+  return (
+    <Card padding={0}>
+      <div className="relative h-25">
+        <div className="relative w-full h-22 rounded-md">
+          <img
+            src={landscape}
+            alt="landscape"
+            className="rounded-t-md h-full w-full "
+          />
+        </div>
+        <div className="absolute top-14 left-6 z-10 ">
+          <img
+            src={profileImage}
+            alt="profileImage"
+            className="rounded-full border-2 h-16 w-16 border-white cursor-pointer "
+          />
+        </div>
+      </div>
+      <div className="p-5 ">
+        <div className="text-xl">User name</div>
+        <div className="text-sm my-1">@Job</div>
+        <div className="text-sm my-1">City</div>
+        <div className="text-sm my-1">Working At</div>
+      </div>
+    </Card>
+  );
+};
+export default ProfileCard;
--- frontend/src/pages/Feeds/Feeds.jsx
+++ frontend/src/pages/Feeds/Feeds.jsx
@@ -1,4 +1,91 @@
+import Card from "../../components/Card/Card";
+import ProfileCard from "../../components/ProfileCard/ProfileCard";
+import profileImage from "../../assets/images/profileImage.png";
+import VideoCallIcon from "@mui/icons-material/VideoCall";
+import PhotoIcon from "@mui/icons-material/Photo";
+import ArticleIcon from "@mui/icons-material/Article";
+import Advertisement from "../../components/Advertisement/Advertisement";
+
 const Feeds = () => {
-  return <div>Feeds</div>;
+  return (
+    <div className="px-5 xl:px-50 py-9 flex gap-5 w-full mt-5 bg-gray-100 ">
+      {/* Left Side */}
+      <div className="w-[21%] sm:block sm:w-[23%] hidden py-5 ">
+        <div className="h-fit">
+          <ProfileCard />
+        </div>
+        <div className="w-full my-5">
+          <Card padding={1}>
+            <div className="w-full flex justify-between ">
+              <div>Profile Viewers</div>
+              <div className="text-blue-900">30</div>
+            </div>
+            <div className="w-full flex justify-between ">
+              <div>Post Impressions</div>
+              <div className="text-blue-900">120</div>
+            </div>
+          </Card>
+        </div>
+      </div>
+
+      {/* Center Side */}
+      <div className="w-[100%] py-5 sm:w-[50%] ">
+        {/* Post Section */}
+        <div>
+          <Card padding={1}>
+            <div className=" flex gap-2 items-center ">
+              <img
+                src={profileImage}
+                alt="profileImage"
+                className=" rounded-4xl w-13 h-1/3 border-2 border-white cursor-progress"
+              />
+              <div className="w-full border-1 p-3 rounded-3xl cursor-pointer hover:bg-gray-100  ">
+                Start a post
+              </div>
+            </div>
+            <div className="w-full flex mt-3 ">
+              <div className="flex gap-2 p-2 cursor-pointer justify-center rounded-lg w-[33%] hover:bg-gray-100">
+                <VideoCallIcon sx={{ color: "green" }} />
+                Video
+              </div>
+              <div className="flex gap-2 p-2 cursor-pointer justify-center rounded-lg w-[33%] hover:bg-gray-100">
+                <PhotoIcon sx={{ color: "blue" }} />
+                Photo
+              </div>
+              <div className="flex gap-2 p-2 cursor-pointer justify-center rounded-lg w-[33%] hover:bg-gray-100">
+                <ArticleIcon sx={{ color: "orange" }} />
+                Article
+              </div>
+            </div>
+          </Card>
+        </div>
+
+        <div className="border-b-1 border-gray-400 w-[100%] my-5 " />
+
+        <div className="w-full flex flex-col gap-5 ">Post</div>
+      </div>
+
+      {/* Right Side */}
+      <div className="w-[26%] py-5 hidden md:block ">
+        <div>
+          <Card padding={1}>
+            <div className="text-xl">LinkedIn News</div>
+            <div className="text-gray-600">Top stories</div>
+            <div className="my-1">
+              <div className="text-md">Buffet to remain Ber chair</div>
+              <div className="text-xs text-gray-400">5h ago</div>
+            </div>
+            <div className="my-1">
+              <div className="text-md">Buffet chair</div>
+              <div className="text-xs text-gray-400">7h ago</div>
+            </div>
+          </Card>
+        </div>
+        <div className="sticky my-5 top-19 ">
+          <Advertisement />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Feeds;
