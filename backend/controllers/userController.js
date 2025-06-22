29970eb8777324efce2d33f3b43be6f25dbf4843
mohammad-rammal <mohammad.rammal@hotmail.com>
2025-06-22 21:57:57 +0300

    feat: implement friend request functionality; add routes and controller methods for sending, accepting, and managing friend requests

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const Notification = require("../models/notification");
 
 /****************************************
  * @desc     Update User
@@ -43,6 +44,221 @@ exports.getProfileById = async (req, res) => {
     res.status(200).json({
       message: "User fetched successfully",
       user: isExist,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error", message: error.message });
+  }
+};
+
+/****************************************
+ * @desc     Find User (Search)
+ * @route    GET /api/user/findUser
+ * @access   Private
+ ****************************************/
+exports.findUser = async (req, res) => {
+  try {
+    const { query } = req.query;
+    const users = await User.find({
+      $and: [
+        { _id: { $ne: req.user._id } },
+        {
+          $or: [
+            { name: { $regex: new RegExp(`^${query}`, "i") } },
+            { email: { $regex: new RegExp(`^${query}`, "i") } },
+          ],
+        },
+      ],
+    });
+
+    res.status(201).json({
+      message: "fetched member",
+      user: users,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error", message: error.message });
+  }
+};
+
+/****************************************
+ * @desc     Send Friend Request
+ * @route    POST /api/user/sendFriendRequest
+ * @access   Private
+ ****************************************/
+exports.sendFriendRequest = async (req, res) => {
+  try {
+    const sender = req.user._id;
+    const { receiver } = req.body;
+
+    const userExist = await User.findById(receiver);
+    if (!userExist) {
+      return res.status(400).json({ error: "User not found!" });
+    }
+    const index = req.user.friends.findIndex((id) => id.equals(receiver));
+    if (index !== -1) {
+      return res.status(400).json({
+        error: "Already friend",
+      });
+    }
+
+    const lastIndex = userExist.pendingFriends.findIndex((id) =>
+      id.equals(req.user._id)
+    );
+    if (lastIndex !== -1) {
+      return res.status(400).json({
+        error: "Already sent request",
+      });
+    }
+
+    userExist.pendingFriends.push(sender);
+    let content = `${req.user.fullName} has send you friend request.`;
+
+    const notification = new Notification({
+      sender,
+      receiver,
+      content,
+      type: "friendRequest",
+    });
+
+    await notification.save();
+    await userExist.save();
+
+    res.status(200).json({
+      message: "Friend request sent",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error", message: error.message });
+  }
+};
+
+/****************************************
+ * @desc     Accept Friend Request
+ * @route    POST /api/user/sendFriendRequest
+ * @access   Private
+ ****************************************/
+exports.acceptFriendRequest = async (req, res) => {
+  try {
+    const { friendId } = req.body;
+    const selfId = req.user._id;
+    console.log(friendId);
+
+    const friendData = await User.findById(friendId);
+    if (!friendData) {
+      return res.status(400).json({ error: "User not found!" });
+    }
+
+    const index = req.user.pendingFriends.findIndex((id) =>
+      id.equals(friendId)
+    );
+    if (index !== -1) {
+      req.user.pendingFriends.splice(index, 1);
+    } else {
+      return res.status(400).json({
+        error: "No request from this such user!",
+      });
+    }
+
+    req.user.friends.push(friendId);
+
+    friendData.friends.push(req.user._id);
+
+    let content = `${req.user.fullName} has accepted your friend request.`;
+
+    const notification = new Notification({
+      sender: req.user._id,
+      receiver: friendId,
+      content,
+      type: "friendRequest",
+    });
+
+    await notification.save();
+    await friendData.save();
+    await req.user.save();
+
+    res.status(200).json({
+      message: "You both are connected now",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error", message: error.message });
+  }
+};
+
+/****************************************
+ * @desc     Get Friends List
+ * @route    GET /api/user/getFriendsList
+ * @access   Private
+ ****************************************/
+exports.getFriendsList = async (req, res) => {
+  try {
+    const friendsList = await req.user.populate("friends");
+    return res.status(200).json({
+      friends: friendsList.friends,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error", message: error.message });
+  }
+};
+
+/****************************************
+ * @desc     Get Pending Friends List
+ * @route    GET /api/user/getPendingFriendsList
+ * @access   Private
+ ****************************************/
+exports.getPendingFriendsList = async (req, res) => {
+  try {
+    const pendingFriendsList = await req.user.populate("pendingFriends");
+    return res.status(200).json({
+      pendingFriends: pendingFriendsList.pendingFriends,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error", message: error.message });
+  }
+};
+
+/****************************************
+ * @desc     Remove From Friend List
+ * @route    DELETE /api/user/removeFromFriend
+ * @access   Private
+ ****************************************/
+exports.removeFromFriend = async (req, res) => {
+  try {
+    const selfId = req.user._id;
+    const { friendId } = req.params;
+
+    const friendData = await User.findById(friendId);
+    if (!friendData) {
+      return res.status(400).json({ error: "User not found!" });
+    }
+
+    const index = req.user.friends.findIndex((id) => id.equals(friendId));
+    const friendIndex = friendData.friends.findIndex((id) => id.equals(selfId));
+
+    if (index !== -1) {
+      req.user.friends.splice(index, 1);
+    } else {
+      return res.status(400).json({
+        error: "No request from such user!",
+      });
+    }
+
+    if (friendIndex !== -1) {
+      friendData.friends.splice(friendIndex, 1);
+    } else {
+      return res.status(400).json({
+        error: "No request from such user!",
+      });
+    }
+
+    await req.user.save();
+    await friendData.save();
+
+    return res.status(200).json({
+      message: "You both are disconnected now",
     });
   } catch (error) {
     console.log(error);
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,6 +4,21 @@ const UserController = require("../controllers/userController");
 const Authentication = require("../authentication/auth");
 
 // /api/user
+router.get("/findUser", Authentication.auth, UserController.findUser);
+router.post(
+  "/sendFriendRequest",
+  Authentication.auth,
+  UserController.sendFriendRequest
+);
+router.post(
+  "/acceptFriendRequest",
+  Authentication.auth,
+  UserController.acceptFriendRequest
+);
+router.get("/friendsList", Authentication.auth, UserController.getFriendsList);
+router.get("/pendingFriendsList", Authentication.auth, UserController.getPendingFriendsList);
+router.delete("/removeFromFriendList/:friendId", Authentication.auth, UserController.removeFromFriend);
+
 router.put("/update", Authentication.auth, UserController.updateUser);
 router.get("/:id", UserController.getProfileById);
 
